dab15adb6a48614305a1c48d99c0fb0a0b121568
Veronica Aranibar Flores <varaniba@f0r2s12.codam.nl>
2026-04-14 13:40:34 +0200

    added first working version of ft_printf and a main.c for testing

---
+++ ft_printf.c
@@ -0,0 +1,127 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_printf.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: varaniba <varaniba@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2026/04/09 14:31:51 by varaniba      #+#    #+#                 */
+/*   Updated: 2026/04/13 19:27:10 by varaniba      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include <stdio.h>
+
+int	print_char(int c)
+{
+	ft_putchar_fd(c, 1);
+	return(1);
+}
+
+int	print_str(char *str)
+{
+	ft_putstr_fd(str, 1);
+	return(ft_strlen(str));
+}
+
+int	putnbr_base_signed(signed int nbr, char *base)
+{
+	long	n;
+	char	c;
+	int		len_base;
+	int		index;
+	int 	count;
+	
+	count = 0;
+	len_base = ft_strlen(base);
+	n = nbr;
+	if (n < 0)
+	{
+		write (1, "-", 1);
+		n = -n;
+		count ++;
+	}
+	if (n >= len_base)
+		count += putnbr_base_signed(n / len_base, base);
+	index = n % len_base;
+	c = base[index];
+	count += write (1, &c, 1);
+	return (count);
+}
+
+int	putnbr_base_unsigned(unsigned long long nbr, char *base)
+{
+	char			c;
+	unsigned int	len_base;
+	int				index;
+	int				count;
+	
+	count = 0;
+	len_base = ft_strlen(base);
+	if (nbr >= len_base)
+		count += putnbr_base_unsigned(nbr / len_base, base);
+	index = nbr % len_base;
+	c = base[index];
+	count += write (1, &c, 1);
+	return (count);
+}
+
+
+int	print_ptr(unsigned long long ptr)
+{
+	int	count;
+	
+	count = 0;
+	if (!ptr)
+		count += write(1, "(nil)", 5);
+	else
+	{
+		count += write(1, "0x", 2);
+		count += putnbr_base_unsigned(ptr, "0123456789abcdef");
+	}
+	return (count);
+}
+
+void	format(char format_c, va_list args, int *counter)
+{
+	if (format_c == 'c')
+		*counter += print_char(va_arg(args, int));
+	else if (format_c == 's')
+		*counter += print_str(va_arg(args, char *));
+	else if (format_c == 'd' || format_c == 'i')
+		*counter += putnbr_base_signed(va_arg(args, signed int), "0123456789");
+	else if (format_c == 'u' )
+		*counter += putnbr_base_unsigned(va_arg(args, unsigned int), "0123456789");
+	else if (format_c == 'x')
+		*counter += putnbr_base_unsigned(va_arg(args, unsigned int), "0123456789abcdef");
+	else if (format_c == 'X')
+		*counter += putnbr_base_unsigned(va_arg(args, unsigned int), "0123456789ABCDEF");
+	else if (format_c == 'p')
+		*counter += print_ptr(va_arg(args, uintptr_t));
+}
+
+int	ft_printf(const char *format_str, ...)
+{
+	va_list	args;
+	va_start(args, format_str);
+	int		i;
+	int		counter;
+	
+	i = 0;
+	counter = 0;
+	while(format_str[i])
+	{
+		if(format_str[i] == '%' && format_str[i + 1] != '%')
+			format(format_str[++i], args, &counter);
+		else
+		{
+			write(1, &format_str[i], 1);
+			counter++;
+		}
+		i++;
+	}
+	va_end(args);
+	return(counter);
+}
+
---
+++ main.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: varaniba <varaniba@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2026/04/13 17:14:24 by varaniba      #+#    #+#                 */
+/*   Updated: 2026/04/13 19:22:02 by varaniba      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "ft_printf.h"
+
+int	main(void)
+{
+	int i;
+	int n;
+
+	printf("\n\n>>> CHARACTER (%%c) <<<\n");
+
+	printf("printf > \n");
+	i =	printf("%c", 'a');
+	printf("\ni = %d\n\n", i);
+
+	printf("ft_printf > \n");
+	n =	ft_printf("%c", 'a');
+	printf("\nn = %d\n\n", n);
+
+	printf("\n\n>>> STRING (%%s) <<<\n");
+	char str[] = "hello";
+
+	printf("printf > \n");
+	i =	printf("%s", str);
+	printf("\ni = %d\n\n", i);
+
+	printf("ft_printf > \n");
+	n =	ft_printf("%s", str);
+	printf("\nn = %d\n\n", n);
+
+
+	printf("\n\n>>> (SIGNED) DECIMAL INTEGER (%%d) <<<\n");
+	int a = -4002;
+
+	printf("printf > \n");
+	i =	printf("%d", a);
+	printf("\ni = %d\n\n", i);
+
+	printf("ft_printf > \n");
+	n =	ft_printf("%d", a);
+	printf("\nn = %d\n\n", n);
+
+
+	printf("\n\n>>> (SIGNED) INTEGER (%%i) <<<\n");
+	int b = -4002;
+
+	printf("printf > \n");
+	i =	printf("%i", b);
+	printf("\ni = %d\n\n", i);
+
+	printf("ft_printf > \n");
+	n =	ft_printf("%i", b);
+	printf("\nn = %d\n\n", n);
+
+
+	printf("\n\n>>> UNSIGNED DECIMAL (%%u) <<<\n");
+	int c = -42;
+
+	printf("printf > \n");
+	i =	printf("%u", c);
+	printf("\ni = %d\n\n", i);
+
+	printf("ft_printf > \n");
+	n =	ft_printf("%u", c);
+	printf("\nn = %d\n\n", n);
+
+
+	printf("\n\n>>> UNSIGNED HEXADECIMAL (lowercase) (%%x) <<<\n");
+	int d = -42;
+
+	printf("printf > \n");
+	i =	printf("%x", d);
+	printf("\ni = %d\n\n", i);
+
+	printf("ft_printf > \n");
+	n =	ft_printf("%x", d);
+	printf("\nn = %d\n\n", n);
+
+
+	printf("\n\n>>> UNSIGNED HEXADECIMAL (upppercase) (%%X) <<<\n");
+	int e = -42;
+
+	printf("printf > \n");
+	i =	printf("%X", e);
+	printf("\ni = %d\n\n", i);
+
+	printf("ft_printf > \n");
+	n =	ft_printf("%X", e);
+	printf("\nn = %d\n\n", n);
+	
+	printf("\n\n>>> ADDRESS OF A POINTER (%%p) <<<\n");
+	char *ptr = NULL;
+
+	printf("printf > \n");
+	i =	printf("%p", ptr);
+	printf("\ni = %d\n\n", i);
+
+	printf("ft_printf > \n");
+	n =	ft_printf("%p", ptr);
+	printf("\nn = %d\n\n", n);
+
+	return (0);
+}
